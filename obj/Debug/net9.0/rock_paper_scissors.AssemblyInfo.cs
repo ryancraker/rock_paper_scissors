6a99cf412a8b344b53b8e23e56918c01fe4428c9
ryancraker <ryan.craker98@gmail.com>
2025-07-28 16:03:20 -0600

    save data change?

--- obj/Debug/net9.0/rock_paper_scissors.AssemblyInfo.cs
+++ obj/Debug/net9.0/rock_paper_scissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e16d151ee14356e457723af15b03db92602f22d")]
 [assembly: System.Reflection.AssemblyProductAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rock_paper_scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
